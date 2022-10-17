10dc1a61971e8d725c733a42c791aa6c8944380c
anggi.hasian.ft20@mail.umy.ac.id <KAMPUNGbilah1112>
2022-10-17 21:26:57 +0700

    add entity class

---
+++ src/main/java/pws_c/connectionDB_PABD/exceptions/PreexistingEntityException.java
@@ -0,0 +1,10 @@
+package pws_c.connectionDB_PABD.exceptions;
+
+public class PreexistingEntityException extends Exception {
+    public PreexistingEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public PreexistingEntityException(String message) {
+        super(message);
+    }
+}
