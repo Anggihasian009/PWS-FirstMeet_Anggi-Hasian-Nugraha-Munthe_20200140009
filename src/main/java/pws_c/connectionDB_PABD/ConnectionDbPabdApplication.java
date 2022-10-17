fe1b01641c2c90eaceb4b51d9c8474881a90d797
anggi.hasian.ft20@mail.umy.ac.id <KAMPUNGbilah1112>
2022-10-17 21:06:48 +0700

    import start springbot

---
+++ src/main/java/pws_c/connectionDB_PABD/ConnectionDbPabdApplication.java
@@ -0,0 +1,13 @@
+package pws_c.connectionDB_PABD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectionDbPabdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectionDbPabdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pws_c/connectionDB_PABD/ConnectionDbPabdApplicationTests.java
@@ -0,0 +1,13 @@
+package pws_c.connectionDB_PABD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectionDbPabdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
